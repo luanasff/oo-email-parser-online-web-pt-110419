f32b666620fb4a19150cbcc714cd8e016e7a9eee
Luana Faria Lane <luanasfff@gmail.com>
2019-12-17 00:34:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,5 +5,18 @@
 
    
  
+class EmailAddressParser
+  attr_accessor :emails, :csv_emails
 
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end
 
+  def parse
+    csv_emails.split.collect do |address|
+      address.split(',')
+    end
+    .flatten.uniq
+  end
+  
+  
